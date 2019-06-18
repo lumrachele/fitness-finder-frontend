7022978d68dad175899b1f5f1942592341cc6d7b
lumrachele <rlum894@gmail.com>
2019-06-18 14:38:46 -0400

    login component

---
+++ components/login.js
@@ -0,0 +1,20 @@
+import React, { Component} from 'react';
+
+export default class Login extends React.Component{
+  state={
+
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>
+          Login Page
+        </h1>
+      </div>
+
+    )
+  }
+
+
+}
