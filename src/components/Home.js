4ff41628aa8887962f81dff0fa642e210958722f
lumrachele <rlum894@gmail.com>
2019-06-27 23:12:09 -0400

    favorites list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 // import React, {Component} from 'react';
 import React from 'react'
+import Favorites from './Favorites.js'
 
-import API_URL from '../constants.js'
+// import API_URL from '../constants.js'
 
 export default function Home (props){
   return(
     <div>
       <h1>Home page</h1>
+      <Favorites/>
+
     </div>
   )
 }
