e797e82240780795770c53b13c6cce76c49626dd
lumrachele <rlum894@gmail.com>
2019-07-07 16:45:36 -0400

    remove button for events from favs

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -6,17 +6,24 @@ export default function EventCard(props){
 
   const eventDetails = function (){
     if(props.event){
-      return (<>
+      return (
+        <>
           <h1>{props.event.title}</h1>
           <p>{props.event.description}</p>
           <ul>
             <li>{props.event.date}</li>
             <li>{props.event.time}</li>
           </ul>
+          <button id="remove" onClick={handleClick}>remove</button>
         </>
       )
     }
-}
+  }
+
+  const handleClick = (e)=>{
+    e.preventDefault()
+    console.log("remove")
+  }
 
 
 
