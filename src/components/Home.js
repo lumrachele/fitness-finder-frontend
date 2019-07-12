cf6538f3363360b8c3229e12d93d1507b82c7c5f
lumrachele <rlum894@gmail.com>
2019-07-12 19:36:04 -0400

    css flex grid styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 // import React, {Component} from 'react';
 import React, {useState, useEffect} from 'react';
+import '../stylesheets/Home.css'
 import Favorites from './Favorites.js'
 
 import {API_URL} from '../constants.js'
@@ -17,10 +18,14 @@ export default function Home (props){
 
 
   return(
-    <div className="home">
-      <h1>Welcome, {user.name}</h1>
-      <Favorites user={user}/>
+    <div id="home">
+      <header><h1>Welcome, {user.name}</h1></header>
 
+      <Favorites user={user}/>
+      <div>sidebar1</div>
+      <div>sidebar2</div>
+
+      <footer><h1>footer</h1></footer>
     </div>
   )
 }
