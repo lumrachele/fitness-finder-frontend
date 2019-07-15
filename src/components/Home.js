eabf9aa3b32a8de1cf917cdb7ab61e824504949c
lumrachele <rlum894@gmail.com>
2019-07-14 23:46:44 -0400

    disable map for now. add search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@
 import React, {useState, useEffect} from 'react';
 import '../stylesheets/Home.css'
 import Favorites from './Favorites.js'
-import Map from './Map.js'
+// import Map from './Map.js'
 
 import {API_URL} from '../constants.js'
 
@@ -20,13 +20,22 @@ export default function Home (props){
 
   return(
     <div id="home">
-      <header><h1>Welcome, {user.name}</h1></header>
+      <header>
+      <h1>Welcome, {user.name}</h1>
+
+      </header>
 
       <Favorites user={user}/>
-      <Map />
-      <div>sidebar2</div>
+      <div>map</div>
+      <div id="search">
+        <form>
+          <input type='text' placeholder="search for fitness venues..."></input>
+        </form>
+      </div>
 
       <footer><h1>footer</h1></footer>
     </div>
   )
 }
+
+// <Map />
