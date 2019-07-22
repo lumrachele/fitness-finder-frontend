b68aff325c9f67762eae6820068f94aec457b000
lumrachele <rlum894@gmail.com>
2019-07-21 20:54:18 -0400

    scrollable favorites list

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,7 +6,7 @@ export default function SearchResults (props){
 
   return(
     <div id="results">
-    <h1>Results from Search</h1>
+  
     {props.results.map(r=>{
       return(<div key={r.id} className="result-card" id={r.id}>{r}</div>)
     })}
