803408a8a8e3e4d85367e7c0f9d541a8910f749a
lumrachele <rlum894@gmail.com>
2019-07-28 21:44:24 -0400

    align items baseline

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react'
 import SearchResults from './SearchResults.js'
 
+import {API_URL} from '../constants.js'
+
 export default function SearchBar(props){
   const [input, setInput] = useState('')
   const [results, setResults] = useState(['crossfit', 'orangetheory', 'rumble'])
@@ -15,8 +17,14 @@ export default function SearchBar(props){
 
     // call the fn in a set timeout after a keypress
 
+// want to make sure that this only runs once on the last keypress
+// debouncing?
     const handleSearch = function(e){
-      console.log(input)
+      // only if time reaches 3 seconds after keypress, run the set timeout search function
+      // setTimeout(()=>{console.log("hit search")}, 3000)
+      //otherwise, render some loading animation
+
+      // fetch(``)
     }
 
 
